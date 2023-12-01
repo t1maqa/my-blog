9373d14b6c88f18b866923fa58b77254fd0599dd
Timur <tim.akhmet@gmail.com>
2023-12-01 16:36:33 +0500

    changes

--- first-project/mysite/mysite/settings.py
+++ first-project/mysite/mysite/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
